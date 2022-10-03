e45bd10675c2d71299152b4c1a79dea71f56913b
senaitekubai <senaitekubai@gmail.com>
2022-10-03 11:51:52 +0100

    added return statements for classes

--- src/main/java/org/example/Player1.java
+++ src/main/java/org/example/Player1.java
@@ -11,59 +11,74 @@ public class Player1 {
         this.score = score;
     }
 
-    public void hitByGun1() {
+    public String hitByGun1() {
+        String message;
         int reducer = 30;
-
-        if (this.score <= 0) {
+        if (this.score <= 0 || (this.score -= reducer) <= 0) {
             this.score = 0;
-            System.out.println("Game over, score is now " + this.score + " " + this.name + " is dead");
+            // System.out.println("Game over, score is now " + this.score + " " + this.name + " is dead");
+            message = "Game over, score is now " + this.score + " " + this.name + " is dead";
         } else {
             this.score -= reducer;
-            System.out.println(this.name + " is hit by Gun1, score is reduced by " + reducer + ", Score is now " + this.score);
+            System.out.println(this.score -=reducer);
 
+            //System.out.println(this.name + " is hit by Gun1, score is reduced by " + reducer + ", Score is now " + this.score);
+            message = this.name + " is hit by Gun1, score is reduced by " + reducer + ", Score is now " + this.score;
         }
-
-
+        return message;
     }
 
-    public void hitOpponentWithGun1() {
-        if(this.score <= 0){
-            System.out.println("score is less than or equal to zero, need to heal first");
-        }else{
-            int increase = 40;
-            this.score +=increase;
-            System.out.println(this.name + " hit opponent with gun1, score is now increased by " + increase + " score is now," + this.score);
-
-        }
-        }
-
-    public void hitOpponentWithGun2() {
-        if(this.score <= 0){
-            System.out.println("score is less than or equal to zero, need to heal first");
-        }else{
-            int increase = 50;
-            this.score +=increase;
-            System.out.println(this.name + " hit opponent with gun2, score is now increased by " + increase + " score is now," + this.score);
-
-        }
-    }
-
-
-    public void hitByGun2() {
-        int reducer = 40;
+    public String hitOpponentWithGun1() {
+        String message;
         if (this.score <= 0) {
+            System.out.println("score is less than or equal to zero, need to heal first");
+            message = ("score is less than or equal to zero, need to heal first");
+        } else {
+            int increase = 40;
+            this.score += increase;
+            System.out.println(this.name + " hit opponent with gun1, score is now increased by " + increase + " score is now " + this.score);
+            message = (this.name + " hit opponent with gun1, score is now increased by " + increase + " score is now " + this.score);
+        }
+
+        return message;
+    }
+
+    public String hitOpponentWithGun2() {
+        String message = "";
+        if (this.score <= 0) {
+            System.out.println("score is less than or equal to zero, need to heal first");
+            message = ("score is less than or equal to zero, need to heal first");
+        } else {
+            int increase = 50;
+            this.score += increase;
+            System.out.println(this.name + " hit opponent with gun2, score is now increased by " + increase + " score is now," + this.score);
+            message = (this.name + " hit opponent with gun2, score is now increased by " + increase + " score is now," + this.score);
+        }
+        return message;
+    }
+
+    public String hitByGun2() {
+        String message = "";
+        int reducer = 40;
+        if (this.score <= 0 || (this.score -= reducer) <= 0) {
             this.score = 0;
             System.out.println("Game over, score is now " + this.score + " " + this.name + " is dead");
+            message = "Game over, score is now " + this.score + " " + this.name + " is dead";
         } else {
             this.score -= reducer;
             System.out.println(this.name + " is hit by Gun2, score is reduced by " + reducer + ", Score is now " + this.score);
-
+            message = this.name + " is hit by Gun2, score is reduced by " + reducer + ", Score is now " + this.score;
         }
-
-
+        return message;
     }
 
-    public void heal() {
-        this.score = 100;
+    public int heal() {
+        int newScore = this.score = 100;
+        return newScore;
+    }
+
+    public String printMessageWhenPlayerIsDead() {
+        System.out.println("Game over, score is now " + this.score + " " + this.name + " is dead");
+        return "Game over, score is now " + this.score + " " + this.name + " is dead";
     }
 }
--- src/main/java/org/example/Player2.java
+++ src/main/java/org/example/Player2.java
@@ -7,42 +7,103 @@ public class Player2 extends Player1 {
 
     public Player2(String name, String weapon, int score, boolean armour) {
         super(name, weapon, score);
-        this.score=score;
-        this.name=name;
+        this.score = score;
+        this.name = name;
         this.armour = armour;
     }
 
     @Override
-    public void printMessageWhenPlayerIsDead() {
-        System.out.println("Game over, score is now " + this.score + " " + this.name + " is dead");
+    public String printMessageWhenPlayerIsDead() {
+        // System.out.println("Game over, score is now " + this.score + " " + this.name + " is dead");
+        return "Game over, score is now " + this.score + " " + this.name + " is dead";
     }
 
     @Override
-    public void hitByGun1() {
-        int reducer;
+    public String hitByGun1() {
+        String message = "";
         if (armour) {
-            reducer = 10;
-            if (this.score <= 0) {
+            int reducer = 10;
+            if (this.score <= 0 || (this.score -= reducer) <= 0) {
                 this.score = 0;
-             printMessageWhenPlayerIsDead();
+                message = printMessageWhenPlayerIsDead();
             } else {
                 this.score -= reducer;
-                System.out.println(" Armour is on, "+this.name + " is hit by Gun1, score is reduced by " + reducer + ", Score is now " + this.score);
-
+                // System.out.println(" Armour is on, " + this.name + " is hit by Gun1, score is reduced by " + reducer + ", Score is now " + this.score);
+                message = " Armour is on, " + this.name + " is hit by Gun1, score is reduced by " + reducer + ", Score is now " + this.score;
             }
-
-
         }
         if (!armour) {
-            reducer = 30;
+            int reducer = 30;
             if (this.score <= 0) {
                 this.score = 0;
-             printMessageWhenPlayerIsDead();
+                message = printMessageWhenPlayerIsDead();
+            } else {
+                this.score -= reducer;
+                //  System.out.println(this.name + " is hit by Gun1, score is reduced by " + reducer + ", Score is now " + this.score);
+                message = this.name + " is hit by Gun1, score is reduced by " + reducer + ", Score is now " + this.score;
+            }
+        }
+        return message;
+    }
+
+    @Override
+    public String hitByGun2() {
+        String message = "";
+        int reducer;
+        if (armour) {
+            reducer = 20;
+            if (this.score <= 0 || (this.score -= reducer) <= 0) {
+                this.score = 0;
+                message = printMessageWhenPlayerIsDead();
+            } else {
+                this.score -= reducer;
+                System.out.println(" Armour is on, " + this.name + " is hit by Gun1, score is reduced by " + reducer + ", Score is now " + this.score);
+                message = " Armour is on, " + this.name + " is hit by Gun1, score is reduced by " + reducer + ", Score is now " + this.score;
+            }
+        }
+
+        if (!armour) {
+            reducer = 40;
+            if (this.score <= 0) {
+                this.score = 0;
+                message = printMessageWhenPlayerIsDead();
             } else {
                 this.score -= reducer;
                 System.out.println(this.name + " is hit by Gun1, score is reduced by " + reducer + ", Score is now " + this.score);
-
+                message = " Armour is on, " + this.name + " is hit by Gun1, score is reduced by " + reducer + ", Score is now " + this.score;
             }
         }
+        return message;
+    }
+
+    @Override
+    public String hitOpponentWithGun1() {
+        String message;
+        if (this.score <= 0) {
+            System.out.println("score is less than or equal to zero, need to heal first");
+            message = ("score is less than or equal to zero, need to heal first");
+        } else {
+            int increase = 40;
+            this.score += increase;
+            System.out.println(this.name + " hit opponent with gun1, score is now increased by " + increase + " score is now " + this.score);
+            message = (this.name + " hit opponent with gun1, score is now increased by " + increase + " score is now " + this.score);
+        }
+
+        return message;
+    }
+
+    @Override
+    public String hitOpponentWithGun2() {
+        String message = "";
+        if (this.score <= 0) {
+            System.out.println("score is less than or equal to zero, need to heal first");
+            message = ("score is less than or equal to zero, need to heal first");
+        } else {
+            int increase = 50;
+            this.score += increase;
+            System.out.println(this.name + " hit opponent with gun2, score is now increased by " + increase + " score is now," + this.score);
+            message = (this.name + " hit opponent with gun2, score is now increased by " + increase + " score is now," + this.score);
+        }
+        return message;
     }
 }
