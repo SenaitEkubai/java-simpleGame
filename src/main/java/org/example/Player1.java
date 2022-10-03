752925350c8e807c7a3187e374e531a6857afca8
senaitekubai <senaitekubai@gmail.com>
2022-10-03 12:31:30 +0100

    removed print statements from player1

--- src/main/java/org/example/Player1.java
+++ src/main/java/org/example/Player1.java
@@ -16,12 +16,10 @@ public class Player1 {
     public String hitOpponentWithGun1() {
         String message;
         if (this.score <= 0) {
-            System.out.println("score is less than or equal to zero, need to heal first");
             message = ("score is less than or equal to zero, need to heal first");
         } else {
             int increase = 40;
             this.score += increase;
-            System.out.println(this.name + " hit opponent with gun1, score is now increased by " + increase + " score is now " + this.score);
             message = (this.name + " hit opponent with gun1, score is now increased by " + increase + " score is now " + this.score);
         }
 
@@ -31,12 +29,10 @@ public class Player1 {
     public String hitOpponentWithGun2() {
         String message = "";
         if (this.score <= 0) {
-            System.out.println("score is less than or equal to zero, need to heal first");
             message = ("score is less than or equal to zero, need to heal first");
         } else {
             int increase = 50;
             this.score += increase;
-            System.out.println(this.name + " hit opponent with gun2, score is now increased by " + increase + " score is now," + this.score);
             message = (this.name + " hit opponent with gun2, score is now increased by " + increase + " score is now," + this.score);
         }
         return message;
