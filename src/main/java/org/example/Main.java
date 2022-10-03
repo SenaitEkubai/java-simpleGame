a4e99519f7a27834578d2d1c2ac2a42a344c7b5b
senaitekubai <senaitekubai@gmail.com>
2022-10-03 11:52:24 +0100

    instantiate classes

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,10 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Player1 player1 = new Player1("Sarah", "gun", 40);
+        Player2 player2 = new Player2("Jane", "gun", 10,true);
+
+        // call methods on each player
+        System.out.println(player1.hitByGun1());
     }
 }
